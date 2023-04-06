68b8494fb7729e13f4880504f930c0f2dfac16eb
Anton Artomov <86731073+AntonArtomovNM@users.noreply.github.com>
2023-04-06 11:40:19 +0300

    Add program class

--- CPP/Program.cs
+++ CPP/Program.cs
@@ -1,37 +1,44 @@
 ﻿using CPP.Utils;
 
-Console.WriteLine("Greetings!");
+namespace CPP;
 
-try
+class Program
 {
-    var testPlan = InputHelper.GetTestPlan();
-
-    Console.Clear();
-    Console.WriteLine($"{testPlan.Name}\n\n{testPlan.TaskDescription}\n");
-
-    testPlan.RunDefaultCases();
-
-    Console.WriteLine("\nPress any key to continue");
-    Console.ReadKey();
-
-    Console.Clear();
-    Console.WriteLine($"{testPlan.Name}\n\n{testPlan.TaskDescription}\n");
-
-    bool shouldExit;
-    do
+    public static void Main(string[] args)
     {
-        testPlan.RunUserInputCases();
+        Console.WriteLine("Greetings!");
 
-        Console.WriteLine("\nPress 0 to exit, press any other key to try again");
-        var key = Console.ReadKey();
-        shouldExit = key.KeyChar.Equals('0');
+        try
+        {
+            var testPlan = InputHelper.GetTestPlan();
 
-        Console.WriteLine();
-        Console.WriteLine();
-    } while (!shouldExit);
+            Console.Clear();
+            Console.WriteLine($"{testPlan.Name}\n\n{testPlan.TaskDescription}\n");
+
+            testPlan.RunDefaultCases();
+
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine($"{testPlan.Name}\n\n{testPlan.TaskDescription}\n");
+
+            bool shouldExit;
+            do
+            {
+                testPlan.RunUserInputCases();
+
+                Console.WriteLine("\nPress 0 to exit, press any other key to try again");
+                var key = Console.ReadKey();
+                shouldExit = key.KeyChar.Equals('0');
+
+                Console.WriteLine();
+                Console.WriteLine();
+            } while (!shouldExit);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
